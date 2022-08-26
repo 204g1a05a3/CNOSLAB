25127a03e46763699d457e5bdca92710ed4509ee
204g1a05a3 <112165069+204g1a05a3@users.noreply.github.com>
2022-08-26 03:12:46 -0700

    My

---
+++ sample.c
@@ -0,0 +1,15 @@
+/*
+Name of the experiment : Experiment
+Roll no : 204g1a05a3
+Name : SRINIVASA SREE SHARAN
+*/
+
+//source code
+
+#include<stdio.h>
+int main()
+{
+printf("HELLO WORLD");
+  return 0;
+}
+  
